ab438c7c294a6b72c6a09ba0cd9eb9019daa62e0
jhonCosta08 <jhonatancosta08@hotmail.com>
2022-01-27 18:00:43 -0300

    Aplicando padrão DAO ao objeto User e criando Evento de dados alterados

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -21,19 +21,30 @@ export class WhatsAppController {
 
     initAuth() {
         this._firebase.initAuth().then(response => {
-            this._user = new User();
-            let userRef = User.findByEmail(response.user.email);
-            userRef.set({
-                name: response.user.displayName,
-                email: response.user.email,
-                photo: response.user.photoURL
-            }).then(res => {
+            this._user = new User(response.user.email);
+            this._user.on('datachange', data => {
+                document.querySelector('title').innerHTML = data.name + ' - connected';
+                this.el.inputNamePanelEditProfile.innerHTML = data.name;
+                if(data.photo) {
+                    let photo = this.el.imgPanelEditProfile;
+                    photo.src = data.photo;
+                    photo.show();
+                    this.el.imgDefaultPanelEditProfile.hide();
+                    let photo2 = this.el.myPhoto.querySelector('img');
+                    photo2.src = data.photo;
+                    photo2.show();
+                }
+            });
+
+            this._user.name = response.user.displayName;
+            this._user.email = response.user.email;
+            this._user.photo = response.user.photoURL;
+            this._user.save().then(() => {
                 this.el.appContent.css({
                     display: 'flex'
                 });
-            }).catch(err => {
-                console.error('Error: ', err);
             });
+
         }).catch(err => {
             console.error('Error: ', err);
         });
