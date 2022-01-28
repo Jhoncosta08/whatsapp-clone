8e9ae2d3d77c1e892d5f101305ca76ce0c4c8543
jhonCosta08 <jhonatancosta08@hotmail.com>
2022-01-28 10:20:41 -0300

    Adicionando um contato no Whatsapp Clone

--- src/model/User.js
+++ src/model/User.js
@@ -41,4 +41,8 @@ export class User extends Model{
         return User.getRef().doc(email);
     }
 
+    addContact(contact){
+        return User.getRef().doc(this.email).collection('contacts').doc(btoa(contact.email)).set(contact.toJSON());
+    }
+
 }
